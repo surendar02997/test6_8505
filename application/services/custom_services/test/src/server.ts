84f42b4dfd11fdb9931e4b0c8b87caae434fb7dc
surendar02997 <surendar02997>
2021-04-29 12:58:49 +0000

    updated generated code

--- application/services/custom_services/test/src/server.ts
+++ application/services/custom_services/test/src/server.ts
@@ -5,7 +5,7 @@ import * as mongoose from 'mongoose';
 import * as cors from 'cors';
 import { WinstonLogger } from './config/WinstonLogger';
 
-const PORT = 8009;
+const PORT = 8010;
 
 class App {
 
